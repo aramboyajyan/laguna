775dbc8fc2081e2e468f344e8ca1a3802cb21cb5
Topsitemakers <support@topsitemakers.com>
2013-03-18 20:15:42 +0100

    Removing widgets folder, adding function for logging

--- includes/helper.custom.php
+++ includes/helper.custom.php
@@ -10,4 +10,15 @@
  * http://www.topsitemakers.com/
  */
 
-// 
+/**
+ * Log output to the database.
+ */
+function custom_debug_watchdog($output, $type = 'log') {
+  global $wpdb;
+  $query = $wpdb->prepare("INSERT INTO {$wpdb->prefix}_custom_debug (`time`, `type`, `output`) VALUES (%d, '%s', '%s')", array(
+    current_time('timestamp'),
+    $type,
+    $output,
+  ));
+  $wpdb->query($query);
+}
