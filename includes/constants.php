d11a3f88950233ae3775ed0850f959b3344a5900
Topsitemakers <support@topsitemakers.com>
2013-03-21 12:53:45 +0100

    Minor cleanup

--- includes/constants.php
+++ includes/constants.php
@@ -10,3 +10,7 @@
 
 // Shortname will be placed before the admin option names
 define('CUSTOM_DEBUG_SHORTNAME', 'custom_debug_');
+
+// Default log type; this is the value saved in the 'type' column when logging
+// events using custom_debug_watchdog() function.
+define('CUSTOM_DEBUG_DEFAULT_LOG_TYPE', 'log');
