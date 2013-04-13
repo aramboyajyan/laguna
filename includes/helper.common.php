fd704526407530a8593fb0ff546d0ee9f93d7b32
Topsitemakers <support@topsitemakers.com>
2013-04-13 19:01:24 +0200

    adding laguna_is_website helper function

--- includes/helper.common.php
+++ includes/helper.common.php
@@ -228,3 +228,38 @@ function laguna_unpublish_resave_post($post_id) {
   wp_update_post($post);
 }
 endif;
+
+/**
+ * Check if a given string is a URL.
+ *
+ * Taken from Drupal core:
+ * http://api.drupal.org/api/drupal/includes!common.inc/function/valid_url/7
+ */
+if (!function_exists('laguna_is_website')):
+function laguna_is_website($string, $soft = FALSE) {
+  if ($soft) {
+    // We will validate URLs that do not have http/https and www in front of
+    // the domain name. We will simply check if they start with those strings;
+    // if they don't we will append them and let the regular checker do its job.
+    if (substr($string, 0, 7) != 'http://' || substr($string, 0, 8) != 'https://') {
+      $string = 'http://' . $string;
+    }
+  }
+  return (bool) preg_match("
+    /^                                                      # Start at the beginning of the text
+    (?:ftp|https?|feed):\/\/                                # Look for ftp, http, https or feed schemes
+    (?:                                                     # Userinfo (optional) which is typically
+      (?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*      # a username or a username and password
+      (?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@          # combination
+    )?
+    (?:
+      (?:[a-z0-9\-\.]|%[0-9a-f]{2})+                        # A domain name or a IPv4 address
+      |(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\])         # or a well formed IPv6 address
+    )
+    (?::[0-9]+)?                                            # Server port number (optional)
+    (?:[\/|\?]
+      (?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})   # The path and query (optional)
+    *)?
+  $/xi", $string);
+}
+endif;
